39e207e45282f6a3e378aa668340493d8cc75845
Little A Games <developer@littleagames.com>
2025-10-07 11:29:21 -0500

    Add pfwolf.pk3 unzipped to repo

---
+++ pfwolf-pk3/scripts/GlobalUsings.cs
@@ -0,0 +1,5 @@
+global using LittleAGames.PFWolf.SDK;
+global using LittleAGames.PFWolf.SDK.Assets;
+global using LittleAGames.PFWolf.SDK.Components;
+global using LittleAGames.PFWolf.SDK.Models;
+global using LittleAGames.PFWolf.SDK.Scripting;
---
+++ pfwolf-pk3/scripts/actors/Guard.cs
@@ -0,0 +1,6 @@
+public class Guard : WolfensteinActor
+{
+    public Guard(int tileX, int tileY) : base(tileX, tileY)
+    {
+    }
+}
